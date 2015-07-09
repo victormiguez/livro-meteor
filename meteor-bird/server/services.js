82925ae1aa08018705bc145b270563301ec5b59f
Victor Miguez <victorcineis@gmail.com>
2015-07-09 12:57:26 -0300

    autenticação via facebook

---
+++ meteor-bird/server/services.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.upsert(
+  {service: 'facebook'},
+  {
+    $set: {
+      appId: '421913994659224',
+      secret: 'e9509b4f9335ffcbe467bc043da7d0a9'
+    }
+  }
+);
